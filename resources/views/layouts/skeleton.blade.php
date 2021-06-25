da71ec5c8d7e743c84893f5f5cff4085f674fdb1
ahsanf <ahsanfirdaus.2018@student.uny.ac.id>
2021-06-25 18:21:28 +0700

    fix sidebar toggle

--- resources/views/layouts/skeleton.blade.php
+++ resources/views/layouts/skeleton.blade.php
@@ -6,6 +6,7 @@
   <title>@yield('title', 'Home') &mdash; {{ config('app.name') }}</title>
   <meta name="csrf-token" content="{{ csrf_token() }}">
   <script src="https://code.jquery.com/jquery-3.5.1.js"></script>
+
   <link rel="stylesheet" href="{{ mix('css/app.css') }}">
   @stack('stylesheet')
 </head>
@@ -17,9 +18,12 @@
 <script src="{{ mix('js/manifest.js') }}"></script>
 <script src="{{ mix('js/vendor.js') }}"></script>
 <script src="{{ mix('js/app.js') }}"></script>
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+{{-- <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.js"></script> --}}
+<script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
 <script src="https://cdn.datatables.net/1.10.24/js/jquery.dataTables.js"></script>
 <script src="https://cdn.datatables.net/1.10.24/js/dataTables.bootstrap4.min.js"></script>
+{{-- <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.nicescroll/3.7.6/jquery.nicescroll.min.js"></script> --}}
+
 <script type="text/javascript">
 function getWorkerByLocation(){
   var hostApi = "http://10.50.0.20:8000/api/by_location/json";
@@ -40,7 +44,7 @@ function getWorker(){
   var i = 1;
   var table = $("#tworker").DataTable({
     ajax : {
-      url : hostApi, 
+      url : hostApi,
       dataSrc : (json) => {
         return json.data.map((row, index) => {
           row.index = index + 1
