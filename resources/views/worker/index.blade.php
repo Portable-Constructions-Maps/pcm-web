adc4db6ca3eece9b3aa6971d87bf5c06924d7783
Dwi Setiawan <dwisetiawan@linuxmail.org>
2021-06-25 19:46:37 +0700

    fix worker count by location

--- resources/views/worker/index.blade.php
+++ resources/views/worker/index.blade.php
@@ -4,22 +4,7 @@
     <div class="section-header">
       <h1>Daftar Pekerja</h1>
     </div>
-    <div class="section">
-      <div class="row">
-        <div class="col-lg-3 col-md-6 col-sm-6 col-12">
-          <div class="card card-statistic-1">
-              <div class="card-icon bg-primary">
-                  <i class="far fa-user"></i>
-              </div>
-              <div class="card-wrap">
-                  <div class="card-header">
-                      <h4></h4>
-                  </div>
-                  <div class="card-body">
-                  </div>
-              </div>
-          </div>
-      </div>
+    <div class="section" id="data_by_locations"> 
     </div>
     <div class="section-body">
       <h2 class="section-title">List of Workers</h2>
