52b4f0772777bac9b10f6196a1b96bfd23390da6
dstwn <dwisetiawan@linuxmail.org>
2021-08-23 00:36:00 +0700

    add monitor menu

--- resources/views/partials/sidebar.blade.php
+++ resources/views/partials/sidebar.blade.php
@@ -6,7 +6,9 @@
     <a href="#">{{ strtoupper(substr(config('app.name'), 0, 2)) }}</a>
   </div>
   <ul class="sidebar-menu">
-    <li class="menu-header">Manajemen</li>
+    <li class="menu-header">Home</li>
+    <li class="{{ Route::is('monitor.index') ? 'active' : '' }}"><a href="{{route('monitor.index')}}"><i class="fas fa-chart-area"></i> <span>Monitor</span></a></li>
+    <li class="menu-header">Management</li>
     {{-- <li class="{{ request()->is('/') ? 'active' : '' }}"><a class="nav-link" href="{{ url('/home') }}"><i class="fas fa-columns"></i> <span>Dashboard</span></a></li> --}}
     <li class="{{ Route::is('worker.show') ? 'active' : '' }}"><a href="{{route('worker.show')}}"><i class="fas fa-users"></i> <span>Workers</span></a></li>
     {{-- <li class="{{ Route::is('worker.rooms') ? 'active' : '' }}"><a href="{{route('worker.rooms')}}"><i class="fas fa-map-marker-alt"></i> <span>Area</span></a></li> --}}
